c0ad57261a06d58597d9d3872d5e090028e013a8
Iraklis Kourtidis <iraklis@gmail.com>
2021-06-24 19:04:30 -0700

    added signedFractionInBps as well

--- src/main/java/com/rb/nonbiz/types/SignedFraction.java
+++ src/main/java/com/rb/nonbiz/types/SignedFraction.java
@@ -55,6 +55,10 @@ public class SignedFraction extends PreciseValue<SignedFraction> {
     return signedFraction(value / 100);
   }
 
+  public static SignedFraction signedFractionInBps(double value) {
+    return signedFraction(value / 10_000);
+  }
+
   public static SignedFraction signedFraction(long numerator, long denominator) {
     RBPreconditions.checkArgument(denominator > 0);
     return new SignedFraction(new BigDecimal(numerator).divide(new BigDecimal(denominator), DEFAULT_MATH_CONTEXT));
