3f4f8ec1873c7d70ab8270df94d101caa079b57c
Iraklis Kourtidis <iraklis@gmail.com>
2021-08-19 22:04:54 -0700

    added a 13-arg overload

--- src/main/java/com/rb/nonbiz/collections/IidMapSimpleConstructors.java
+++ src/main/java/com/rb/nonbiz/collections/IidMapSimpleConstructors.java
@@ -267,4 +267,34 @@ public class IidMapSimpleConstructors {
     return newIidMap(map);
   }
 
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(13);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    return newIidMap(map);
+  }
+
 }
