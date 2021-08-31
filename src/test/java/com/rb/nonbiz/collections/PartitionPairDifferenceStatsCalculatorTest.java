d8abea8160b25cdff7d56b5e99954fd6da6e011e
Iraklis Kourtidis <iraklis@gmail.com>
2021-08-30 21:22:56 -0700

    IDE rename

--- src/test/java/com/rb/nonbiz/collections/PartitionPairDifferenceStatsCalculatorTest.java
+++ src/test/java/com/rb/nonbiz/collections/PartitionPairDifferenceStatsCalculatorTest.java
@@ -18,7 +18,7 @@ import static com.rb.nonbiz.testutils.Asserters.doubleExplained;
 import static com.rb.nonbiz.types.UnitFraction.unitFraction;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class PartitionPairDifferenceStatsCalculatorTest extends RBTest<PartitionDifferenceStatsCalculator> {
+public class PartitionPairDifferenceStatsCalculatorTest extends RBTest<PartitionPairDifferenceStatsCalculator> {
 
   @Test
   public void generalCase_partitionsHaveSomeOverlap_returnsSum() {
@@ -168,8 +168,8 @@ public class PartitionPairDifferenceStatsCalculatorTest extends RBTest<Partition
   }
 
   @Override
-  protected PartitionDifferenceStatsCalculator makeTestObject() {
-    return new PartitionDifferenceStatsCalculator();
+  protected PartitionPairDifferenceStatsCalculator makeTestObject() {
+    return new PartitionPairDifferenceStatsCalculator();
   }
 
 }
