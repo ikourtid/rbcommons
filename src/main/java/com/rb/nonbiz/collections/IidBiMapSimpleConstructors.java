be333469b528334eac825b0bd420475fa619a175
Stephen Anderson <steve@rowboatadvisors.com>
2021-09-30 17:54:02 -0400

    checkpoint

---
+++ src/main/java/com/rb/nonbiz/collections/IidBiMapSimpleConstructors.java
@@ -0,0 +1,51 @@
+package com.rb.nonbiz.collections;
+
+import com.rb.biz.types.asset.InstrumentId;
+
+import static com.rb.nonbiz.collections.IidBiMap.iidBiMap;
+import static com.rb.nonbiz.collections.IidMapSimpleConstructors.emptyIidMap;
+import static com.rb.nonbiz.collections.IidMapSimpleConstructors.newIidMap;
+import static com.rb.nonbiz.collections.IidMapSimpleConstructors.singletonIidMap;
+import static com.rb.nonbiz.collections.MutableIidMap.newMutableIidMapWithExpectedSize;
+
+public class IidBiMaps {
+
+  public static <V> IidBiMap<V> emptyIidBiMap() {
+    return iidBiMap(emptyIidMap());
+  }
+
+  public static <V> IidBiMap<V> singletonIidBiMap(InstrumentId instrumentId, V item) {
+    return iidBiMap(singletonIidMap(instrumentId, item));
+  }
+
+  public static <V> IidBiMap<V> iidBiMapOf(InstrumentId k1, V v1,
+                                           InstrumentId k2, V v2) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(2);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    return iidBiMap(newIidMap(map));
+  }
+
+  public static <V> IidBiMap<V> iidBiMapOf(InstrumentId k1, V v1,
+                                           InstrumentId k2, V v2,
+                                           InstrumentId k3, V v3) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(3);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    return iidBiMap(newIidMap(map));
+  }
+
+  public static <V> IidBiMap<V> iidBiMapOf(InstrumentId k1, V v1,
+                                           InstrumentId k2, V v2,
+                                           InstrumentId k3, V v3,
+                                           InstrumentId k4, V v4) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(4);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    return iidBiMap(newIidMap(map));
+  }
+
+}
