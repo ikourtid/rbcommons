2eb9f96093ac1735e376617c3e8c438d7b63c823
Iraklis Kourtidis <iraklis@gmail.com>
2021-10-26 16:53:43 -0700

    added 16-arg rbmapOf

--- src/main/java/com/rb/nonbiz/collections/RBMapSimpleConstructors.java
+++ src/main/java/com/rb/nonbiz/collections/RBMapSimpleConstructors.java
@@ -180,4 +180,25 @@ public class RBMapSimpleConstructors {
         .build());
   }
 
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14) {
+    return newRBMap(new ImmutableMap.Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .build());
+  }
+
 }
