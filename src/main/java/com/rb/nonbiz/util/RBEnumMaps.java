a3d05917bfb24cac5a82d44d8f644f396ed1d3b6
Iraklis Kourtidis <iraklis@gmail.com>
2022-03-12 17:30:11 -0800

    added overload

--- src/main/java/com/rb/nonbiz/util/RBEnumMaps.java
+++ src/main/java/com/rb/nonbiz/util/RBEnumMaps.java
@@ -2,13 +2,16 @@ package com.rb.nonbiz.util;
 
 import com.rb.nonbiz.collections.MutableRBMap;
 import com.rb.nonbiz.collections.RBMap;
+import com.rb.nonbiz.collections.RBMapConstructors;
 import com.rb.nonbiz.collections.RBSet;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static com.rb.nonbiz.collections.MutableRBMap.newMutableRBMapWithExpectedSize;
+import static com.rb.nonbiz.collections.RBMapConstructors.rbMapFromStream;
 import static com.rb.nonbiz.collections.RBSet.newRBSet;
 import static com.rb.nonbiz.collections.RBSet.rbSetOf;
 
@@ -47,6 +50,21 @@ public class RBEnumMaps {
         "All values for enum %s must appear in the map: enum has %s ; map is %s",
         enumClass, enumValues, valuesMap);
     return new EnumMap<E, V>(valuesMap.asMap());
+  }
+
+  /**
+   * Creates an EnumMap such that every enum constant appears in the map.
+   * Each value is a function of the key specified.
+   */
+  public static <E extends Enum<E>, V> EnumMap<E, V> enumMapCoveringAllEnumValues(
+      Class<E> enumClass,
+      Function<E, V> valueGenerator) {
+    RBPreconditions.checkArgument(enumClass.isEnum());
+    return new EnumMap<E, V>(rbMapFromStream(
+        Arrays.stream(enumClass.getEnumConstants()),
+        key -> key,
+        valueGenerator)
+        .asMap());
   }
 
 }
