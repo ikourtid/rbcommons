ac96929cc0c6fbc6b7236cf849e32a47c7a307a0
Iraklis Kourtidis <iraklis@gmail.com>
2022-06-12 20:22:05 -0700

    comment

--- src/main/java/com/rb/nonbiz/jsonapi/HasJsonApiDocumentation.java
+++ src/main/java/com/rb/nonbiz/jsonapi/HasJsonApiDocumentation.java
@@ -1,6 +1,9 @@
 package com.rb.nonbiz.jsonapi;
 
-// FIXME IAK JSONDOC
+/**
+ * Used by JSON API converters (NOT the classes they convert) to denote that a class has human-readable documentation
+ * (typically for developers) for its JSON format.
+ */
 public interface HasJsonApiDocumentation<T> {
 
   JsonApiDocumentation<T> getJsonApiDocumentation();
