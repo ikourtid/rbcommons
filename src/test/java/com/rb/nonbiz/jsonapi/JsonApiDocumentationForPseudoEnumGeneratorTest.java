cb9b8268924d78374caa31e581059bed7176143e
Iraklis Kourtidis <iraklis@gmail.com>
2022-08-13 11:02:04 -0700

    added JsonApiDocumentationForPseudoEnumGeneratorTest

--- src/test/java/com/rb/nonbiz/jsonapi/JsonApiDocumentationForPseudoEnumGeneratorTest.java
+++ src/test/java/com/rb/nonbiz/jsonapi/JsonApiDocumentationForPseudoEnumGeneratorTest.java
@@ -1,19 +1,54 @@
 package com.rb.nonbiz.jsonapi;
 
+import com.rb.biz.types.asset.InstrumentType;
+import com.rb.nonbiz.testutils.RBTest;
+import com.rb.nonbiz.testutils.TestEnumXYZ;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static com.rb.nonbiz.collections.RBMapSimpleConstructors.rbMapOf;
+import static com.rb.nonbiz.json.DataClassJsonApiDescriptor.PseudoEnumJsonApiDescriptor.pseudoEnumJsonApiDescriptor;
+import static com.rb.nonbiz.json.DataClassJsonApiDescriptor.PseudoEnumJsonApiDescriptor.undefinedPseudoEnumJsonApiDescription;
+import static com.rb.nonbiz.jsonapi.JsonApiDocumentation.JsonApiDocumentationBuilder.jsonApiDocumentationBuilder;
+import static com.rb.nonbiz.jsonapi.JsonApiDocumentationTest.jsonApiDocumentationMatcher;
+import static com.rb.nonbiz.text.SimpleHumanReadableLabel.label;
+import static com.rb.nonbiz.text.Strings.asSingleLine;
+import static com.rb.nonbiz.util.RBEnumMapSimpleConstructors.enumMapOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.hamcrest.TypeSafeMatcher;
-
-import static com.rb.nonbiz.testmatchers.Match.match;
 import static com.rb.nonbiz.testmatchers.RBMatchers.makeMatcher;
 
-public class JsonApiDocumentationForPseudoEnumGeneratorTest {
+public class JsonApiDocumentationForPseudoEnumGeneratorTest extends RBTest<JsonApiDocumentationForPseudoEnumGenerator> {
 
   @Test
-  public void reminder() {
-    fail("");
+  public void testTextGeneration() {
+    assertThat(
+        makeTestObject().generate(
+            InstrumentType.class,
+            label("One of several security types."),
+            "This test only supports ETFs and stocks.",
+            pseudoEnumJsonApiDescriptor(rbMapOf(
+                "is_etf",   label("Must also have 'etf' key with a `EtfInstrumentType` in its contents"),
+                "is_stock", label("Must also have 'stock' key with a `StockInstrumentType` in its contents")))),
+        jsonApiDocumentationMatcher(
+            jsonApiDocumentationBuilder()
+                .setClass(InstrumentType.class)
+                .setSingleLineSummary(label("One of several security types."))
+                .setLongDocumentation(asSingleLine(
+                    "<p> This test only supports ETFs and stocks. </p>\n",
+                    "<p> The following values are valid:\n<ul>",
+                    "<li> <strong>is_etf</strong> : Must also have 'etf' key with a `EtfInstrumentType` in its contents </li>\n",
+                    "<li> <strong>is_stock</strong> : Must also have 'stock' key with a `StockInstrumentType` in its contents </li>\n",
+                    "</ul></p>\n"))
+                .hasNoJsonValidationInstructions()
+                .hasNoChildNodes()
+                .noTrivialSampleJsonSupplied()
+                .noNontrivialSampleJsonSupplied()
+                .build()));
+  }
+
+  @Override
+  protected JsonApiDocumentationForPseudoEnumGenerator makeTestObject() {
+    return new JsonApiDocumentationForPseudoEnumGenerator();
   }
 
 }
