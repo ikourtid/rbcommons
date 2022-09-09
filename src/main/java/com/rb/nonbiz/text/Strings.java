0d1f92af9798278ab2c67efaaa13d17b3ff0cf36
Iraklis Kourtidis <iraklis@gmail.com>
2022-09-08 21:39:24 -0700

    adding spaces

--- src/main/java/com/rb/nonbiz/text/Strings.java
+++ src/main/java/com/rb/nonbiz/text/Strings.java
@@ -13,8 +13,10 @@ import com.rb.nonbiz.collections.IidMap;
 import com.rb.nonbiz.collections.ImmutableIndexableArray1D;
 import com.rb.nonbiz.collections.NonZeroDeviations;
 import com.rb.nonbiz.collections.Partition;
+import com.rb.nonbiz.collections.RBIterators;
 import com.rb.nonbiz.collections.RBMap;
 import com.rb.nonbiz.types.UnitFraction;
+import org.checkerframework.checker.units.qual.C;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -31,6 +33,7 @@ import static com.rb.biz.marketdata.instrumentmaster.InstrumentMasters.displaySy
 import static com.rb.biz.marketdata.instrumentmaster.NullInstrumentMaster.NULL_INSTRUMENT_MASTER;
 import static com.rb.nonbiz.date.RBDates.UNUSED_DATE;
 import static java.lang.Character.isUpperCase;
+import static java.lang.Character.isWhitespace;
 import static java.util.Comparator.comparing;
 import static java.util.Comparator.reverseOrder;
 import static java.util.Map.Entry.comparingByKey;
@@ -401,13 +404,50 @@ public class Strings {
    * This is handy in situations where we need to create a multiline string,
    * but want to avoid the ugliness of having plus signs in the Java code for concatenation,
    * which messes up indentation.
+   *
+   * <p> This also adds a space between any pair of consecutive lines, in the event that concatenating them
+   * would result in concatenating two words. This method is used for human-readable text, so if the code author
+   * forgot to add spaces between lines, we don't want </p>
    */
-  public static String asSingleLine(String first, String... rest) {
+  public static String asSingleLine(String first, String ... rest) {
     StringBuilder sb = new StringBuilder(first);
-    for (String s : rest) {
+    if (!lastCharacterIsWhitespace(first) && rest.length > 0 && !firstCharacterIsWhitespace(rest[0])) {
+      // concatenating the first two lines ('first' and 'rest[0]') might end up joining two words and making the
+      // text illegible, so let's add a space, although ideally the caller would avoid that in the first place.
+      sb.append(' ');
+    }
+    for (int i = 0; i < rest.length; i++) {
+      String s = rest[i];
       sb.append(s);
+      // Similar to previous comment. If the current line doesn't end in whitespace, and there's another line after
+      // it, and that next line doesn't start with a whitespace, add a space between the two lines.
+      if (!lastCharacterIsWhitespace(s) && i < rest.length - 1 && !firstCharacterIsWhitespace(rest[i + 1])) {
+        sb.append(' ');
+      }
     }
     return sb.toString();
+  }
+
+  /**
+   * Returns true if the last character of the string is a whitespace character (space, tab, newline, etc.)
+   * Returns false for an empty string, since an empty string doesn't have a last character.
+   */
+  public static boolean lastCharacterIsWhitespace(String s) {
+    if (s.isEmpty()) {
+      return false;
+    }
+    return isWhitespace(s.charAt(s.length() - 1));
+  }
+
+  /**
+   * Returns true if the last character of the string is a whitespace character (space, tab, newline, etc.)
+   * Returns false for an empty string, since an empty string doesn't have a last character.
+   */
+  public static boolean firstCharacterIsWhitespace(String s) {
+    if (s.isEmpty()) {
+      return false;
+    }
+    return isWhitespace(s.charAt(0));
   }
 
   /**
--- src/test/java/com/rb/nonbiz/text/StringsTest.java
+++ src/test/java/com/rb/nonbiz/text/StringsTest.java
@@ -18,16 +18,20 @@ import static com.rb.nonbiz.collections.RBSet.rbSetOf;
 import static com.rb.nonbiz.date.RBDates.UNUSED_DATE;
 import static com.rb.nonbiz.text.Strings.asSingleLine;
 import static com.rb.nonbiz.text.Strings.asSingleLineWithNewlines;
+import static com.rb.nonbiz.text.Strings.firstCharacterIsWhitespace;
 import static com.rb.nonbiz.text.Strings.formatCollectionInOrder;
 import static com.rb.nonbiz.text.Strings.formatMapInKeyOrder;
 import static com.rb.nonbiz.text.Strings.formatOptionalPrintsInstruments;
 import static com.rb.nonbiz.text.Strings.joinWithHarvardComma;
+import static com.rb.nonbiz.text.Strings.lastCharacterIsWhitespace;
 import static com.rb.nonbiz.text.Strings.toTrimmedStandaloneSentence;
 import static com.rb.nonbiz.types.PreciseValue.formatWithoutCommas;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.reverseOrder;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class StringsTest {
 
@@ -168,14 +172,54 @@ public class StringsTest {
 
   @Test
   public void testAsSingleLine() {
-    assertEquals("ab", asSingleLine("a", "b"));
-    assertEquals("abc", asSingleLine("a", "b", "c"));
+    // Simple cases, where we don't need to add a space between lines
+
+    // You would think that we should disallow this by changing the method signature to take 2 non-varargs arguments
+    // (String first, String second, String ... rest), but that doesn't work, because there are places where we
+    // don't know for sure if we have more than 1 string.
+    assertEquals("a",      asSingleLine("a"));
+
+    assertEquals("a b",    asSingleLine("a ", "b"));
+    assertEquals("a\tb",   asSingleLine("a\t", "b"));
+    assertEquals("a b",    asSingleLine("a", " b"));
+    assertEquals("a\tb",   asSingleLine("a", "\tb"));
+    assertEquals("a  b",   asSingleLine("a ", " b"));
+    assertEquals("a\t\tb", asSingleLine("a\t", "\tb"));
+
+    assertEquals("a b c",    asSingleLine("a ", "b", " c"));
+    assertEquals("a\tb c",   asSingleLine("a\t", "b", " c"));
+    assertEquals("a b c",    asSingleLine("a", " b", " c"));
+    assertEquals("a\tb c",   asSingleLine("a", "\tb", " c"));
+    assertEquals("a  b c",   asSingleLine("a ", " b", " c"));
+    assertEquals("a\t\tb c", asSingleLine("a\t", "\tb", " c"));
+
+    // In the following cases, the method adds a space, in order to avoid concatenating two words into a single one.
+    assertEquals("a b", asSingleLine("a", "b"));
+    assertEquals("a b c", asSingleLine("a", "b", "c"));
   }
 
   @Test
   public void testAsSingleLineWithNewlines() {
     assertEquals("a\nb\n", asSingleLineWithNewlines("a", "b"));
     assertEquals("a\nb\nc\n", asSingleLineWithNewlines("a", "b", "c"));
+  }
+
+  @Test
+  public void testFirstCharacterIsWhitespace() {
+    rbSetOf(" ", "  ", " x", " x ", "\t", "\tx")
+        .forEach(startsWithWhitespace -> assertTrue(firstCharacterIsWhitespace(startsWithWhitespace)));
+
+    rbSetOf("", "x", "x ", "x y")
+        .forEach(doesNotStartWithWhitespace -> assertFalse(firstCharacterIsWhitespace(doesNotStartWithWhitespace)));
+  }
+
+  @Test
+  public void testLastCharacterIsWhitespace() {
+    rbSetOf(" ", "  ", "x ", " x ", "\t", "x\t")
+        .forEach(startsWithWhitespace -> assertTrue(lastCharacterIsWhitespace(startsWithWhitespace)));
+
+    rbSetOf("", "x", " x", "x y")
+        .forEach(doesNotStartWithWhitespace -> assertFalse(lastCharacterIsWhitespace(doesNotStartWithWhitespace)));
   }
 
 }
