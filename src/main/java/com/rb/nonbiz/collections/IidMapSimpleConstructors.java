0b2c2768fd6b408fe681aab64cd9027b53462d4f
Stephen Anderson <steve@rowboatadvisors.com>
2022-10-10 14:56:33 -0400

    update

--- src/main/java/com/rb/nonbiz/collections/IidMapSimpleConstructors.java
+++ src/main/java/com/rb/nonbiz/collections/IidMapSimpleConstructors.java
@@ -1,5 +1,6 @@
 package com.rb.nonbiz.collections;
 
+import com.google.common.collect.ImmutableMap;
 import com.rb.biz.types.asset.InstrumentId;
 import gnu.trove.map.hash.TLongObjectHashMap;
 
@@ -28,8 +29,8 @@ public class IidMapSimpleConstructors {
   }
 
   /**
-   * There are a few cases where we need to create an IidMap based off a plain Map,
-   * e.g. if we use ImmutableMap#builder
+   * There are a few cases where we need to create an {@link IidMap} based off a plain Map,
+   * e.g. if we use {@link ImmutableMap#builder}.
    */
   public static <V> IidMap<V> newIidMap(Map<InstrumentId, V> initialMap) {
     if (initialMap.isEmpty()) {
@@ -294,6 +295,272 @@ public class IidMapSimpleConstructors {
     map.putAssumingAbsent(k11, v11);
     map.putAssumingAbsent(k12, v12);
     map.putAssumingAbsent(k13, v13);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(14);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(15);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15,
+                                       InstrumentId k16, V v16) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(16);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    map.putAssumingAbsent(k16, v16);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15,
+                                       InstrumentId k16, V v16,
+                                       InstrumentId k17, V v17) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(17);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    map.putAssumingAbsent(k16, v16);
+    map.putAssumingAbsent(k17, v17);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15,
+                                       InstrumentId k16, V v16,
+                                       InstrumentId k17, V v17,
+                                       InstrumentId k18, V v18) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(18);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    map.putAssumingAbsent(k16, v16);
+    map.putAssumingAbsent(k17, v17);
+    map.putAssumingAbsent(k18, v18);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15,
+                                       InstrumentId k16, V v16,
+                                       InstrumentId k17, V v17,
+                                       InstrumentId k18, V v18,
+                                       InstrumentId k19, V v19) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(19);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    map.putAssumingAbsent(k16, v16);
+    map.putAssumingAbsent(k17, v17);
+    map.putAssumingAbsent(k18, v18);
+    map.putAssumingAbsent(k19, v19);
+    return newIidMap(map);
+  }
+
+  public static <V> IidMap<V> iidMapOf(InstrumentId k1, V v1,
+                                       InstrumentId k2, V v2,
+                                       InstrumentId k3, V v3,
+                                       InstrumentId k4, V v4,
+                                       InstrumentId k5, V v5,
+                                       InstrumentId k6, V v6,
+                                       InstrumentId k7, V v7,
+                                       InstrumentId k8, V v8,
+                                       InstrumentId k9, V v9,
+                                       InstrumentId k10, V v10,
+                                       InstrumentId k11, V v11,
+                                       InstrumentId k12, V v12,
+                                       InstrumentId k13, V v13,
+                                       InstrumentId k14, V v14,
+                                       InstrumentId k15, V v15,
+                                       InstrumentId k16, V v16,
+                                       InstrumentId k17, V v17,
+                                       InstrumentId k18, V v18,
+                                       InstrumentId k19, V v19,
+                                       InstrumentId k20, V v20) {
+    MutableIidMap<V> map = newMutableIidMapWithExpectedSize(20);
+    map.putAssumingAbsent(k1, v1);
+    map.putAssumingAbsent(k2, v2);
+    map.putAssumingAbsent(k3, v3);
+    map.putAssumingAbsent(k4, v4);
+    map.putAssumingAbsent(k5, v5);
+    map.putAssumingAbsent(k6, v6);
+    map.putAssumingAbsent(k7, v7);
+    map.putAssumingAbsent(k8, v8);
+    map.putAssumingAbsent(k9, v9);
+    map.putAssumingAbsent(k10, v10);
+    map.putAssumingAbsent(k11, v11);
+    map.putAssumingAbsent(k12, v12);
+    map.putAssumingAbsent(k13, v13);
+    map.putAssumingAbsent(k14, v14);
+    map.putAssumingAbsent(k15, v15);
+    map.putAssumingAbsent(k16, v16);
+    map.putAssumingAbsent(k17, v17);
+    map.putAssumingAbsent(k18, v18);
+    map.putAssumingAbsent(k19, v19);
+    map.putAssumingAbsent(k20, v20);
     return newIidMap(map);
   }
 
