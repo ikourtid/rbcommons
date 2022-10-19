93d647fb2027417a693e593f3f63a548b61a0e57
Stephen Anderson <steve@rowboatadvisors.com>
2022-10-18 22:16:17 -0400

    make JsonArrayApiTestPair and JsonArrayApiTestData

---
+++ src/test/java/com/rb/nonbiz/jsonapi/JsonArrayApiTestData.java
@@ -0,0 +1,43 @@
+package com.rb.nonbiz.jsonapi;
+
+import com.rb.nonbiz.testmatchers.RBMatchers.MatcherGenerator;
+
+import java.util.List;
+
+import static com.rb.nonbiz.collections.RBLists.concatenateFirstAndRest;
+
+/**
+ * A collection of pairs of JSON and the corresponding Java objects,
+ * so we can make sure that back-and-forth conversions are correct.
+ */
+public class JsonArrayApiTestData<T> {
+
+  private final MatcherGenerator<T> matcherGenerator;
+  private final List<JsonArrayApiTestPair<T>> testPairs;
+
+  private JsonArrayApiTestData
+      (MatcherGenerator<T> matcherGenerator,
+       List<JsonArrayApiTestPair<T>> testPairs) {
+    this.matcherGenerator = matcherGenerator;
+    this.testPairs = testPairs;
+  }
+
+  @SafeVarargs
+  public static <T> JsonArrayApiTestData<T> jsonApiTestData(
+      MatcherGenerator<T> matcherGenerator,
+      JsonArrayApiTestPair<T> first,
+      JsonArrayApiTestPair<T> ... rest) {
+    return new JsonArrayApiTestData<>(
+        matcherGenerator,
+        concatenateFirstAndRest(first, rest));
+  }
+
+  public List<JsonArrayApiTestPair<T>> getTestPairs() {
+    return testPairs;
+  }
+
+  public MatcherGenerator<T> getMatcherGenerator() {
+    return matcherGenerator;
+  }
+
+}
