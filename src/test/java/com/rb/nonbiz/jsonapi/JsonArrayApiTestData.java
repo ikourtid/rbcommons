b4bd264c08b1ac62ba7aa945ae04dbca9e6d0501
Stephen Anderson <steve@rowboatadvisors.com>
2022-10-20 22:30:56 -0400

    cleanup

--- src/test/java/com/rb/nonbiz/jsonapi/JsonArrayApiTestData.java
+++ src/test/java/com/rb/nonbiz/jsonapi/JsonArrayApiTestData.java
@@ -7,17 +7,19 @@ import java.util.List;
 import static com.rb.nonbiz.collections.RBLists.concatenateFirstAndRest;
 
 /**
- * A collection of pairs of JSON arrays and the corresponding Java objects,
+ * A collection of pairs of JSON arrays and their corresponding Java objects,
  * so we can make sure that back-and-forth conversions are correct.
+ *
+ * @see JsonArrayApiTestPair
  */
 public class JsonArrayApiTestData<T> {
 
   private final MatcherGenerator<T> matcherGenerator;
   private final List<JsonArrayApiTestPair<T>> testPairs;
 
-  private JsonArrayApiTestData
-      (MatcherGenerator<T> matcherGenerator,
-       List<JsonArrayApiTestPair<T>> testPairs) {
+  private JsonArrayApiTestData(
+      MatcherGenerator<T> matcherGenerator,
+      List<JsonArrayApiTestPair<T>> testPairs) {
     this.matcherGenerator = matcherGenerator;
     this.testPairs = testPairs;
   }
