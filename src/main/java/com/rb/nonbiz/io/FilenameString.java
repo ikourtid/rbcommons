4f99bbf9f94b99ce380f2aa1f9335acfc33e0c46
Chris Murray <chris@rowboatadvisors.com>
2022-12-01 13:21:19 -0800

    add file name string

---
+++ src/main/java/com/rb/nonbiz/io/FilenameString.java
@@ -0,0 +1,14 @@
+package com.rb.nonbiz.io;
+
+public class FilenameString {
+  private String Filename;
+
+  public FilenameString(String filename) {
+    this.Filename = filename;
+  }
+
+  public String getFilename() {
+    return this.Filename;
+  }
+  
+}
