61d27d7a0b92b2b743d84ea22b0dd8731c3b2c41
Chris Murray <chris@rowboatadvisors.com>
2022-12-02 09:32:14 -0800

    update filename class with style conventions

---
+++ src/main/java/com/rb/nonbiz/io/Filename.java
@@ -0,0 +1,26 @@
+package com.rb.nonbiz.io;
+
+
+/*
+This holds a filename.  It's intentionally light-weight and doesn't do a lot.
+The main reason it exists is to make filenames more typesafe as function arguments.
+This class is very much not intended to do on-disk validation of files, checking that
+files exist, are open, are or aren't directories, etc.
+ */
+public class Filename {
+
+  private final String rawFilenameString;
+
+  private Filename(String filename) {
+    this.rawFilenameString  = filename;
+  }
+
+  public static Filename filename(String rawString){
+    return new Filename(rawString);
+  }
+
+  public String getFilename() {
+    return rawFilenameString;
+  }
+  
+}
---
+++ src/test/java/com/rb/nonbiz/io/FilenameTest.java
@@ -0,0 +1,40 @@
+package com.rb.nonbiz.io;
+
+import com.rb.nonbiz.testutils.RBTestMatcher;
+import org.junit.Test;
+
+import static com.rb.nonbiz.io.Filename.filename;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
+public class FilenameTest extends RBTestMatcher<Filename> {
+
+  @Test
+  public void testGetFilename() {
+    Filename myFilename = filename("out.txt");
+    assertEquals("out.txt", myFilename.getFilename());
+    assertNotSame("someotherstring", myFilename.getFilename());
+  }
+
+  // I really don't have a strong opinion on what makes a filename trivial, so this is a short filename
+  @Override
+  public Filename makeTrivialObject() {
+    return filename("dummy.txt");
+  }
+
+  @Override
+  public Filename makeNontrivialObject() {
+    return filename("this_is_another_dummy.txt");
+  }
+
+  @Override
+  public Filename makeMatchingNontrivialObject() {
+    return makeNontrivialObject();
+  }
+
+  @Override
+  protected boolean willMatch(Filename expected, Filename actual) {
+    return expected.getFilename() == actual.getFilename();
+  }
+
+}
