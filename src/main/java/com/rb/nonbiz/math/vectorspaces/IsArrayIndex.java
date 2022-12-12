579786d411ce3557e3c31c705c9328e829777912
Iraklis Kourtidis <iraklis@gmail.com>
2022-12-12 13:36:58 -0800

    checkpoint

--- src/main/java/com/rb/nonbiz/math/vectorspaces/IsArrayIndex.java
+++ src/main/java/com/rb/nonbiz/math/vectorspaces/IsArrayIndex.java
@@ -1,5 +1,11 @@
 package com.rb.nonbiz.math.vectorspaces;
 
+/**
+ * This must get implemented by any simple class that's meant to be a thin wrapper around an array index.
+ *
+ * <p> Currently (Dec 2022), this is {@link MatrixRowIndex} and {@link MatrixColumnIndex}, but if we were to add
+ * a simple 'array index' wrapper (to avoid using int), that would also have to implement this interface </p>
+ */
 public interface IsArrayIndex {
 
   int asInt();
