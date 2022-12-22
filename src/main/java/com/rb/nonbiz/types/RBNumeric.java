a629a3ba8a55041287eb3795d1d32102891f9c64
Iraklis Kourtidis <iraklis@gmail.com>
2022-12-22 07:55:37 -0800

    no need for interface

--- src/main/java/com/rb/nonbiz/types/RBNumeric.java
+++ src/main/java/com/rb/nonbiz/types/RBNumeric.java
@@ -5,7 +5,30 @@ package com.rb.nonbiz.types;
  * that work both on them. Moreover, because RBNumeric extends Number, it means that we can *also* write methods that
  * can work on all 3 of PreciseValue, ImpreciseValue, and Double - although those may look a bit hacky because
  * technically they should also support long, int, etc., which also extend Number.
+ *
+ * <p> Moreover, Java's {@link Comparable} interface has well-defined semantics, but it's hard to read.
+ * {@code foo1.compareTo(foo2) < 0 }
+ * is harder to read than
+ * {@code foo1.isLessThan(foo2) }
+ *
+ * This class adds some intuitive comparison methods that are easier to read. </p>
  */
-public abstract class RBNumeric<T> extends Number implements IntuitiveComparable<T> {
+public abstract class RBNumeric<T> extends Number implements Comparable<T> {
+
+  public boolean isGreaterThan(T other) {
+    return this.compareTo(other) > 0;
+  }
+
+  public boolean isGreaterThanOrEqualTo(T other) {
+    return this.compareTo(other) >= 0;
+  }
+
+  public boolean isLessThan(T other) {
+    return this.compareTo(other) < 0;
+  }
+
+  public boolean isLessThanOrEqualTo(T other) {
+    return this.compareTo(other) <= 0;
+  }
 
 }
