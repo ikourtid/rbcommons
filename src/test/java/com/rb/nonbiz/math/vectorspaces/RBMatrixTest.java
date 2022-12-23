71778e978ac7602cbce00236d81e4c696036fef9
Stephen Anderson <steve@rowboatadvisors.com>
2022-12-23 09:48:57 -0500

    PR comment

--- src/test/java/com/rb/nonbiz/math/vectorspaces/RBMatrixTest.java
+++ src/test/java/com/rb/nonbiz/math/vectorspaces/RBMatrixTest.java
@@ -193,20 +193,27 @@ public class RBMatrixTest extends RBTestMatcher<RBMatrix> {
 
   @Test
   public void testGetIJ() {
-    RBMatrix matrix = rbMatrix2by2(
-        1.0, 2.0,
-        3.0, 4.0);
+    RBMatrix matrix = rbMatrix(new double[][] {
+        { 1.0, 2.0 },
+        { 3.0, 4.0 },
+        { 5.0, 6.0 }});
     assertEquals(1.0, matrix.get(0, 0), 1e-8);
     assertEquals(2.0, matrix.get(0, 1), 1e-8);
     assertEquals(3.0, matrix.get(1, 0), 1e-8);
     assertEquals(4.0, matrix.get(1, 1), 1e-8);
+    assertEquals(5.0, matrix.get(2, 0), 1e-8);
+    assertEquals(6.0, matrix.get(2, 1), 1e-8);
 
     // no negative indices
     assertIndexOutOfBoundsException( () -> matrix.get(-1,  0));
     assertIndexOutOfBoundsException( () -> matrix.get( 0, -1));
+
     // indices can't be too large
     assertIndexOutOfBoundsException( () -> matrix.get(0, 2));
-    assertIndexOutOfBoundsException( () -> matrix.get(2, 0));
+    assertIndexOutOfBoundsException( () -> matrix.get(1, 2));
+    assertIndexOutOfBoundsException( () -> matrix.get(2, 2));
+    assertIndexOutOfBoundsException( () -> matrix.get(3, 0));
+    assertIndexOutOfBoundsException( () -> matrix.get(3, 1));
   }
 
   @Test
