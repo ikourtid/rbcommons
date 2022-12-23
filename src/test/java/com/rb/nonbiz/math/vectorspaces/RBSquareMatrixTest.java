15e51eadee5463daeb32b4c00f3c5a887d2e624d
Stephen Anderson <steve@rowboatadvisors.com>
2022-12-23 09:49:39 -0500

    add a test
    add a convenience function

--- src/test/java/com/rb/nonbiz/math/vectorspaces/RBSquareMatrixTest.java
+++ src/test/java/com/rb/nonbiz/math/vectorspaces/RBSquareMatrixTest.java
@@ -6,7 +6,10 @@ import com.rb.nonbiz.testutils.RBTestMatcher;
 import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 
+import static com.rb.nonbiz.math.vectorspaces.RBMatrix.rbDiagonalMatrix;
+import static com.rb.nonbiz.math.vectorspaces.RBMatrix.rbIdentityMatrix;
 import static com.rb.nonbiz.math.vectorspaces.RBMatrix.rbMatrix;
+import static com.rb.nonbiz.math.vectorspaces.RBMatrixTest.rbMatrix2by2;
 import static com.rb.nonbiz.math.vectorspaces.RBMatrixTest.rbMatrixMatcher;
 import static com.rb.nonbiz.math.vectorspaces.RBSquareMatrix.rbDiagonalSquareMatrix;
 import static com.rb.nonbiz.math.vectorspaces.RBSquareMatrix.rbIdentitySquareMatrix;
@@ -14,14 +17,37 @@ import static com.rb.nonbiz.math.vectorspaces.RBVectorTest.rbVector;
 import static com.rb.nonbiz.testmatchers.Match.match;
 import static com.rb.nonbiz.testmatchers.RBMatchers.makeMatcher;
 import static com.rb.nonbiz.testutils.Asserters.assertIllegalArgumentException;
+import static com.rb.nonbiz.testutils.Asserters.assertIndexOutOfBoundsException;
 import static com.rb.nonbiz.testutils.Epsilons.emptyEpsilons;
 import static com.rb.nonbiz.testutils.Epsilons.useEpsilonEverywhere;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class RBSquareMatrixTest extends RBTestMatcher<RBSquareMatrix> {
 
   public static RBSquareMatrix singletonRBSquareMatrix(double onlyValue) {
     return rbSquareMatrix(new double[][] { { onlyValue } });
+  }
+
+  public static RBSquareMatrix rbSquareMatrixIdentity(int n) {
+    return RBSquareMatrix.rbSquareMatrix(rbIdentityMatrix(n));
+  }
+
+  public static RBSquareMatrix rbSquareMatrixDiagonal(RBVector rbVector) {
+    return RBSquareMatrix.rbSquareMatrix(rbDiagonalMatrix(rbVector));
+  }
+
+  public static RBSquareMatrix rbSquareMatrixDiagonal2by2(double a11, double a22) {
+    return rbSquareMatrix2by2(
+        a11,  0,
+        0,  a22);
+  }
+
+  public static RBSquareMatrix rbSquareMatrixDiagonal3by3(double a11, double a22, double a33) {
+    return rbSquareMatrix3by3(
+        a11,  0,   0,
+        0,  a22,   0,
+        0,    0, a33);
   }
 
   public static RBSquareMatrix rbSquareMatrix2by2(double a11, double a12, double a21, double a22) {
@@ -93,6 +119,24 @@ public class RBSquareMatrixTest extends RBTestMatcher<RBSquareMatrix> {
             77, 0, 0,
             0, 88, 0,
             0, 0, 99)));
+  }
+
+  @Test
+  public void testGetIJ() {
+    RBSquareMatrix matrix = RBSquareMatrix.rbSquareMatrix(rbMatrix2by2(
+        1.0, 2.0,
+        3.0, 4.0));
+    assertEquals(1.0, matrix.get(0, 0), 1e-8);
+    assertEquals(2.0, matrix.get(0, 1), 1e-8);
+    assertEquals(3.0, matrix.get(1, 0), 1e-8);
+    assertEquals(4.0, matrix.get(1, 1), 1e-8);
+
+    // no negative indices
+    assertIndexOutOfBoundsException( () -> matrix.get(-1,  0));
+    assertIndexOutOfBoundsException( () -> matrix.get( 0, -1));
+    // indices can't be too large
+    assertIndexOutOfBoundsException( () -> matrix.get(0, 2));
+    assertIndexOutOfBoundsException( () -> matrix.get(2, 0));
   }
 
   @Override
