e96369ddbc0b40bc046e3f3c1d00a534230fc742
Iraklis Kourtidis <iraklis@gmail.com>
2022-12-26 23:29:15 -0800

    added Epsilon class

---
+++ src/test/java/com/rb/nonbiz/types/EpsilonTest.java
@@ -0,0 +1,19 @@
+package com.rb.nonbiz.types;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.TypeSafeMatcher;
+
+import static com.rb.nonbiz.testmatchers.Match.match;
+import static com.rb.nonbiz.testmatchers.RBMatchers.makeMatcher;
+
+public class EpsilonTest {
+
+  @Test
+  public void reminder() {
+    fail("");
+  }
+
+}
