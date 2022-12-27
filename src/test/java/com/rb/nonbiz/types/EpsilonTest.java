211509ca3b20aeec46dab3287bf4baa8bd0f3ec7
Iraklis Kourtidis <iraklis@gmail.com>
2022-12-26 23:32:47 -0800

    added test

--- src/test/java/com/rb/nonbiz/types/EpsilonTest.java
+++ src/test/java/com/rb/nonbiz/types/EpsilonTest.java
@@ -2,6 +2,8 @@ package com.rb.nonbiz.types;
 
 import org.junit.Test;
 
+import static com.rb.nonbiz.testutils.Asserters.assertIllegalArgumentException;
+import static com.rb.nonbiz.types.Epsilon.epsilon;
 import static org.junit.Assert.*;
 
 import org.hamcrest.TypeSafeMatcher;
@@ -12,8 +14,22 @@ import static com.rb.nonbiz.testmatchers.RBMatchers.makeMatcher;
 public class EpsilonTest {
 
   @Test
-  public void reminder() {
-    fail("");
+  public void testValidValues() {
+    assertIllegalArgumentException( () -> epsilon(-999));
+    assertIllegalArgumentException( () -> epsilon(-1e-9));
+    Epsilon doesNotThrow;
+    doesNotThrow = epsilon(0);
+    doesNotThrow = epsilon(1e-9);
+    doesNotThrow = epsilon(1e-7);
+    doesNotThrow = epsilon(1);
+    doesNotThrow = epsilon(99);
+    assertIllegalArgumentException( () -> epsilon(100));
+  }
+
+  @Test
+  public void testWithin() {
+    assertTrue(epsilon(1e-8).areWithin(100, 100 + 1e-9));
+    assertFalse(epsilon(1e-8).areWithin(100, 100 + 1e-7));
   }
 
 }
