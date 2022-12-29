3f4cc8e299f487c82f4a728904b1cb85d91c009a
Iraklis Kourtidis <iraklis@gmail.com>
2022-12-28 19:11:48 -0800

    added epsilonMatcher

--- src/test/java/com/rb/nonbiz/types/EpsilonTest.java
+++ src/test/java/com/rb/nonbiz/types/EpsilonTest.java
@@ -2,8 +2,10 @@ package com.rb.nonbiz.types;
 
 import org.junit.Test;
 
+import static com.rb.nonbiz.testmatchers.RBValueMatchers.impreciseValueMatcher;
 import static com.rb.nonbiz.testutils.Asserters.assertIllegalArgumentException;
 import static com.rb.nonbiz.types.Epsilon.DEFAULT_EPSILON_1e_8;
+import static com.rb.nonbiz.types.Epsilon.ZERO_EPSILON;
 import static com.rb.nonbiz.types.Epsilon.epsilon;
 import static org.junit.Assert.*;
 
@@ -33,6 +35,18 @@ public class EpsilonTest {
   public void testWithin() {
     assertTrue(DEFAULT_EPSILON_1e_8.areWithin(100, 100 + 1e-9));
     assertFalse(DEFAULT_EPSILON_1e_8.areWithin(100, 100 + 1e-7));
+  }
+
+  /**
+   * {@link Epsilon} extends {@link ImpreciseValue}, which has its own rich set of matchers.
+   * Also, we usually don't have separate matchers for {@link ImpreciseValue} and {@link PreciseValue}.
+   * However, in those few cases where we need to match epsilons, it's clearer to use a named matcher,
+   * otherwise there would be two epsilons: the value to compare against, and also the epsilon used for comparing the
+   * epsilons themselves. Note that we use an epsilon of 0 here, because epsilons are almost never the result of a
+   * calculation that can cause numbers to be off; instead, their numeric literals are specified in the code.
+   */
+  public static TypeSafeMatcher<Epsilon> epsilonMatcher(Epsilon expected) {
+    return impreciseValueMatcher(expected, ZERO_EPSILON);
   }
 
 }
