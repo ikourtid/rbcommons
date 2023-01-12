d06bcb98eb469dee1d7e952fb5a66d9658863295
Iraklis Kourtidis <iraklis@gmail.com>
2023-01-12 00:11:42 -0800

    helper

--- src/main/java/com/rb/nonbiz/collections/HasInstrumentIdSets.java
+++ src/main/java/com/rb/nonbiz/collections/HasInstrumentIdSets.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 
 import static com.rb.nonbiz.collections.IidMapConstructors.iidMapFromStream;
 import static com.rb.nonbiz.collections.MutableIidMap.newMutableIidMapWithExpectedSize;
+import static com.rb.nonbiz.collections.RBStreams.concatenateFirstSecondAndRest;
 
 
 public class HasInstrumentIdSets {
@@ -56,5 +57,16 @@ public class HasInstrumentIdSets {
     }
     return newHasInstrumentIdSet(map);
   }
+
+  @SafeVarargs
+  public static <T extends HasInstrumentId> HasInstrumentIdSet<T> mergeHasInstrumentIdSets(
+      HasInstrumentIdSet<T> first,
+      HasInstrumentIdSet<T> second,
+      HasInstrumentIdSet<T> ... rest) {
+    return newHasInstrumentIdSet(
+        concatenateFirstSecondAndRest(first, second, rest)
+            .flatMap(v -> v.stream()));
+  }
+
 
 }
