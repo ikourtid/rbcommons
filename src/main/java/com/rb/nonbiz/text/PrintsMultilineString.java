78b895c812abb0f5674d28df9d971cb8e611c458
Stephen Anderson <steve@rowboatadvisors.com>
2023-01-16 17:36:41 -0500

    typo

--- src/main/java/com/rb/nonbiz/text/PrintsMultilineString.java
+++ src/main/java/com/rb/nonbiz/text/PrintsMultilineString.java
@@ -7,7 +7,7 @@ package com.rb.nonbiz.text;
  * by date. The downside is that most tools (diff, vi, etc.) don't play well with long horizontal lines.
  * Therefore, it is sometimes convenient to print over multiple lines. A linear program is a great example. </p>
  *
- * <p> This interface make the 'can print a multipline version of toString()' official. </p>
+ * <p> This interface makes the 'can print a multi-line version of toString()' official. </p>
  */
 public interface PrintsMultilineString {
 
