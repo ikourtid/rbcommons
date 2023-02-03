e227caf28e7283892c78cd2698abff02803d8b73
Iraklis Kourtidis <iraklis@gmail.com>
2023-02-02 18:04:52 -0800

    comments

--- src/main/java/com/rb/biz/guice/RBClock.java
+++ src/main/java/com/rb/biz/guice/RBClock.java
@@ -9,8 +9,9 @@ import java.time.ZonedDateTime;
 
 /**
  * Tells us what time it is.
- * We wrap all calls to 'what time is it?' so that we can replace 'wall clock' time (real) with
- * whatever time it happens to be during some multi-year simulation that we run.
+ *
+ * <p> We wrap all calls to 'what time is it?' so that we can replace 'wall clock' time (real) with
+ * whatever time it happens to be during some multi-year simulation that we run. </p>
  */
 public interface RBClock {
 
@@ -25,7 +26,7 @@ public interface RBClock {
 
   default ZonedDateTime nowOnEastCoast() {
     // All production machines should be on East Coast time, because that's kind of standard,
-    // as NYSE is on the East Coast, and all exchanges follow by and large the same trading hours (9:30 - 4).
+    // as NYSE is on the East Coast, and all US exchanges follow by and large the same trading hours (9:30 - 4).
     return now().atZone(EAST_COAST_ZONE_ID);
   }
 
