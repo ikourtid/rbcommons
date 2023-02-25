fcf9d4b0f8a6642d4ab14ecd2165a430d228b76b
Iraklis Kourtidis <iraklis@gmail.com>
2023-02-24 18:49:18 -0800

    works

--- src/main/java/com/rb/nonbiz/text/SmartFormatterHelper.java
+++ src/main/java/com/rb/nonbiz/text/SmartFormatterHelper.java
@@ -5,8 +5,11 @@ import com.rb.biz.guice.RBClock;
 import com.rb.biz.marketdata.instrumentmaster.InstrumentMaster;
 import com.rb.biz.types.Symbol;
 import com.rb.biz.types.asset.InstrumentId;
+import com.rb.nonbiz.types.LongCounter;
 
 import java.util.Arrays;
+
+import static com.rb.nonbiz.types.LongCounter.longCounter;
 
 /**
  * This class is to help exception messages automatically print information about the {@link Symbol}
@@ -22,7 +25,7 @@ public class SmartFormatterHelper {
   @Inject InstrumentMaster instrumentMaster;
   @Inject RBClock rbClock;
 
-  private Long stackDepth = 0L;
+  private final ThreadLocal<LongCounter> stackDepthByThread = ThreadLocal.withInitial( () -> longCounter());
 
   String formatWithDatePrepended(String template, Object ... args) {
     return formatHelper(true, template, args);
@@ -33,47 +36,50 @@ public class SmartFormatterHelper {
   }
 
   synchronized String formatSingleObject(Object obj) {
-    stackDepth++;
-    try {
-      if (instrumentMaster == null || rbClock == null) {
-        return obj.toString();
-      }
+    LongCounter stackDepth = stackDepthByThread.get();
+    stackDepth.increment();
 
-      return PrintsInstruments.class.isAssignableFrom(obj.getClass()) && stackDepth < 10
-          ? ((PrintsInstruments) obj).toString(instrumentMaster, rbClock.today())
-          : obj.toString();
-    } finally {
-      stackDepth--;
+    if (instrumentMaster == null || rbClock == null) {
+      return obj.toString();
     }
+
+    if (obj == null) {
+      return "<null>";
+    }
+    String toReturn = PrintsInstruments.class.isAssignableFrom(obj.getClass()) && stackDepth.get() < 10
+        ? ((PrintsInstruments) obj).toString(instrumentMaster, rbClock.today())
+        : obj.toString();
+
+    stackDepth.decrement();
+    return toReturn;
   }
 
   synchronized private String formatHelper(boolean prependDate, String template, Object ... args) {
-    stackDepth++;
-    try {
-      StringBuilder sb = new StringBuilder();
-      // We never allow for values to stay null. This is an exception. Otherwise,
-      // every unit test for code that logs would have to set the RBClock, which is a pain,
-      // OR we would have to hook up Guice modules for every RBTest - also a pain, and also would make the tests slower.
-      if (prependDate) {
-        sb.append(rbClock == null ? "0000-00-00 " : Strings.format("%s ", rbClock.today()));
-      }
+    LongCounter stackDepth = stackDepthByThread.get();
 
-      if (instrumentMaster == null || rbClock == null) {
-        sb.append(Strings.format(template, args));
-        return sb.toString();
-      }
-
-      if (stackDepth >= 10) {
-        sb.append(Strings.format(template, args));
-      } else {
-        Object[] newArgs = new Object[args.length];
-        Arrays.setAll(newArgs, i -> formatSingleObject(args[i]));
-        sb.append(Strings.format(template, newArgs));
-      }
-      return sb.toString();
-    } finally {
-      stackDepth--;
+    StringBuilder sb = new StringBuilder();
+    // We never allow for values to stay null. This is an exception. Otherwise,
+    // every unit test for code that logs would have to set the RBClock, which is a pain,
+    // OR we would have to hook up Guice modules for every RBTest - also a pain, and also would make the tests slower.
+    if (prependDate) {
+      sb.append(rbClock == null ? "0000-00-00 " : Strings.format("%s ", rbClock.today()));
     }
+
+    if (instrumentMaster == null || rbClock == null) {
+      sb.append(Strings.format(template, args));
+      return sb.toString();
+    }
+
+    if (stackDepth.get() >= 10) {
+      sb.append(Strings.format(template, args));
+    } else {
+      Object[] newArgs = new Object[args.length];
+      Arrays.setAll(newArgs, i -> formatSingleObject(args[i]));
+      sb.append(Strings.format(template, newArgs));
+    }
+    String toReturn = sb.toString();
+    stackDepth.decrement();
+    return toReturn;
   }
 
 }
