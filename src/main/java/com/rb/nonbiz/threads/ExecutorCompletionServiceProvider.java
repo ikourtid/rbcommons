6ce9a9d7502777301a1c95ea35ba9c0f31a21f3d
Iraklis Kourtidis <iraklis@gmail.com>
2023-03-21 22:26:20 -0700

    comments

--- src/main/java/com/rb/nonbiz/threads/ExecutorCompletionServiceProvider.java
+++ src/main/java/com/rb/nonbiz/threads/ExecutorCompletionServiceProvider.java
@@ -7,15 +7,18 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.Executors;
 
 /**
- * Some backtests run on all possible threads (12, on Iraklis's machine);
- * some only on 3 (which is the number that seems to work without causing problems in practice).
- * This allows you to control how many threads will be used, on a per backtest basis.
+ * Some backtests or daytests run on all possible threads (12, on Iraklis's machine);
+ * some may need fewer, if they are resource-heavy (e.g. maybe we can't fit 12 backtests in memory at
+ * the same time).
+ * This allows you to control how many threads will be used, on a per daytest or backtest basis.
+ * Of course, the concept is more general; it just so happens that currently (Mar 2023) we only use this
+ * for running daytests and backtests in parallel.
  */
 @Singleton
 public class ExecutorCompletionServiceProvider implements AbstractExecutionCompletionServiceProvider {
 
   // This will be 12 for a hexa-core machine and 8 for a quad-core.
-  // This currently works (March 2019) because all machines this runs on have Intel processors with hyperthreading.
+  // This currently works (March 2023) because all machines this runs on have Intel processors with hyperthreading.
   public static final int NUM_CORES_INCLUDING_HYPERTHREADING = Runtime.getRuntime().availableProcessors();
   public static final int NUM_PHYSICAL_CORES = NUM_CORES_INCLUDING_HYPERTHREADING / 2;
 
