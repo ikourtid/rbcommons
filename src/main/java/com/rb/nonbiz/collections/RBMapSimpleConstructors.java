93c181ea274393abc993793f91abdddd615d7528
Stephen Anderson <steve@rowboatadvisors.com>
2023-03-27 19:49:24 -0400

    Add CTORs up to 20 K, V arguments
    check map sizes for key duplicates

--- src/main/java/com/rb/nonbiz/collections/RBMapSimpleConstructors.java
+++ src/main/java/com/rb/nonbiz/collections/RBMapSimpleConstructors.java
@@ -1,6 +1,8 @@
 package com.rb.nonbiz.collections;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableMap.Builder;
+import com.rb.nonbiz.util.RBPreconditions;
 
 import java.util.Map;
 
@@ -10,14 +12,14 @@ public class RBMapSimpleConstructors {
 
   public static <K, V> RBMap<K, V> newRBMap(Map<K, V> rawMap) {
     return rawMap.isEmpty()
-        ? EMPTY_INSTANCE
-        : new RBMap<K, V>(rawMap);
+           ? EMPTY_INSTANCE
+           : new RBMap<K, V>(rawMap);
   }
 
   public static <K, V> RBMap<K, V> newRBMap(MutableRBMap<K, V> mutableRBMap) {
     return mutableRBMap.isEmpty()
-        ? EMPTY_INSTANCE
-        : new RBMap<K, V>(mutableRBMap.asMap());
+           ? EMPTY_INSTANCE
+           : new RBMap<K, V>(mutableRBMap.asMap());
   }
 
   /**
@@ -39,23 +41,31 @@ public class RBMapSimpleConstructors {
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2) {
-    return newRBMap(ImmutableMap.of(k1, v1, k2, v2));
+    RBMap<K, V> rbMap = newRBMap(ImmutableMap.of(k1, v1, k2, v2));
+    checkSize(rbMap, 2);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3) {
-    return newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3));
+    RBMap<K, V> rbMap = newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3));
+    checkSize(rbMap, 3);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4) {
-    return newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3, k4, v4));
+    RBMap<K, V> rbMap = newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3, k4, v4));
+    checkSize(rbMap, 4);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5) {
-    return newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5));
+    RBMap<K, V> rbMap = newRBMap(ImmutableMap.of(k1, v1, k2, v2, k3, v3, k4, v4, k5, v5));
+    checkSize(rbMap, 5);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5, K k6, V v6) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -63,11 +73,13 @@ public class RBMapSimpleConstructors {
         .put(k5, v5)
         .put(k6, v6)
         .build());
+    checkSize(rbMap, 6);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -76,11 +88,13 @@ public class RBMapSimpleConstructors {
         .put(k6, v6)
         .put(k7, v7)
         .build());
+    checkSize(rbMap, 7);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -90,11 +104,13 @@ public class RBMapSimpleConstructors {
         .put(k7, v7)
         .put(k8, v8)
         .build());
+    checkSize(rbMap, 8);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -105,11 +121,13 @@ public class RBMapSimpleConstructors {
         .put(k8, v8)
         .put(k9, v9)
         .build());
+    checkSize(rbMap, 9);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -121,12 +139,14 @@ public class RBMapSimpleConstructors {
         .put(k9, v9)
         .put(k10, v10)
         .build());
+    checkSize(rbMap, 10);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
                                            K k11, V v11) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -139,12 +159,14 @@ public class RBMapSimpleConstructors {
         .put(k10, v10)
         .put(k11, v11)
         .build());
+    checkSize(rbMap, 11);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
                                            K k11, V v11, K k12, V v12) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -158,12 +180,14 @@ public class RBMapSimpleConstructors {
         .put(k11, v11)
         .put(k12, v12)
         .build());
+    checkSize(rbMap, 12);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
                                            K k11, V v11, K k12, V v12, K k13, V v13) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -178,12 +202,14 @@ public class RBMapSimpleConstructors {
         .put(k12, v12)
         .put(k13, v13)
         .build());
+    checkSize(rbMap, 13);
+    return rbMap;
   }
 
   public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
                                            K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
                                            K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14) {
-    return newRBMap(new ImmutableMap.Builder<K, V>()
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
         .put(k1, v1)
         .put(k2, v2)
         .put(k3, v3)
@@ -199,6 +225,184 @@ public class RBMapSimpleConstructors {
         .put(k13, v13)
         .put(k14, v14)
         .build());
+    checkSize(rbMap, 14);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14, K k15, V v15) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .build());
+    checkSize(rbMap, 15);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14,
+                                           K k15, V v15, K k16, V v16) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .put(k16, v16)
+        .build());
+    checkSize(rbMap, 16);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14,
+                                           K k15, V v15, K k16, V v16, K k17, V v17) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .put(k16, v16)
+        .put(k17, v17)
+        .build());
+    checkSize(rbMap, 17);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14,
+                                           K k15, V v15, K k16, V v16, K k17, V v17, K k18, V v18) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .put(k16, v16)
+        .put(k17, v17)
+        .put(k18, v18)
+        .build());
+    checkSize(rbMap, 18);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14,
+                                           K k15, V v15, K k16, V v16, K k17, V v17, K k18, V v18,
+                                           K k19, V v19) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .put(k16, v16)
+        .put(k17, v17)
+        .put(k18, v18)
+        .put(k19, v19)
+        .build());
+    checkSize(rbMap, 19);
+    return rbMap;
+  }
+
+  public static <K, V> RBMap<K, V> rbMapOf(K k1, V v1, K k2, V v2, K k3, V v3, K k4, V v4, K k5, V v5,
+                                           K k6, V v6, K k7, V v7, K k8, V v8, K k9, V v9, K k10, V v10,
+                                           K k11, V v11, K k12, V v12, K k13, V v13, K k14, V v14,
+                                           K k15, V v15, K k16, V v16, K k17, V v17, K k18, V v18,
+                                           K k19, V v19, K k20, V v20) {
+    RBMap<K, V> rbMap = newRBMap(new Builder<K, V>()
+        .put(k1, v1)
+        .put(k2, v2)
+        .put(k3, v3)
+        .put(k4, v4)
+        .put(k5, v5)
+        .put(k6, v6)
+        .put(k7, v7)
+        .put(k8, v8)
+        .put(k9, v9)
+        .put(k10, v10)
+        .put(k11, v11)
+        .put(k12, v12)
+        .put(k13, v13)
+        .put(k14, v14)
+        .put(k15, v15)
+        .put(k16, v16)
+        .put(k17, v17)
+        .put(k18, v18)
+        .put(k19, v19)
+        .put(k20, v20)
+        .build());
+    checkSize(rbMap, 20);
+    return rbMap;
+  }
+
+  // To make sure that no keys are duplicates, check that the size is as expected.
+  // We do this rather than use .putAssumingAbsent() because the Map Builder (from Guava)
+  // does not have putAssumingAbsent().
+  private static <K, V> void checkSize(RBMap<K, V> rbMap, int n) {
+    RBPreconditions.checkArgument(
+        rbMap.size() == n,
+        "RBMap should have been of size %s but had size %s: %s",
+        n, rbMap.size(), rbMap);
   }
 
 }
--- src/test/java/com/rb/nonbiz/collections/RBMapSimpleConstructorsTest.java
+++ src/test/java/com/rb/nonbiz/collections/RBMapSimpleConstructorsTest.java
@@ -3,6 +3,7 @@ package com.rb.nonbiz.collections;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.function.Function;
 
 import static com.rb.nonbiz.collections.MutableRBMap.newMutableRBMap;
 import static com.rb.nonbiz.collections.RBMapMergers.mergeRBMapsDisallowingOverlap;
@@ -28,14 +29,14 @@ public class RBMapSimpleConstructorsTest {
 
   @Test
   public void testSingletonConstructor() {
-    RBMap singletonRBMap = singletonRBMap("key1", 111);
+    RBMap<String, Integer> singletonRBMap = singletonRBMap("key1", 111);
     assertEquals("key1", singletonRBMap.keySet().toArray()[0]);
     assertEquals(111,    singletonRBMap.values().toArray()[0]);
   }
 
   @Test
   public void testMultiArgumentConstructors() {
-    RBMap rbMap12 = rbMapOf(
+    RBMap<String, Integer> rbMap12 = rbMapOf(
         "k1", 1,
         "k2", 2,
         "k3", 3,
@@ -86,6 +87,100 @@ public class RBMapSimpleConstructorsTest {
             rbMapOf("k1", 1, "k2", 2, "k3", 3, "k4", 4, "k5", 5, "k6", 6),
             rbMapOf("k7", 7, "k8", 8, "k9", 9, "k10", 10, "k11", 11, "k12", 12)),
         rbMap12);
+  }
+
+  // More tests after constructors with more arguments were added.
+  @Test
+  public void testMultiArgumentConstructors_part2() {
+    RBMap<String, Integer> rbMap20 = rbMapOf(
+        "k1", 1,
+        "k2", 2,
+        "k3", 3,
+        "k4", 4,
+        "k5", 5,
+        "k6", 6,
+        "k7", 7,
+        "k8", 8,
+        "k9", 9,
+        "k10", 10,
+        "k11", 11,
+        "k12", 12,
+        "k13", 13,
+        "k14", 14,
+        "k15", 15,
+        "k16", 16,
+        "k17", 17,
+        "k18", 18,
+        "k19", 19,
+        "k20", 20);
+
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            singletonRBMap("k1", 1),
+            rbMapOf("k2", 2, "k3", 3, "k4", 4, "k5", 5, "k6", 6, "k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2),
+            rbMapOf("k3", 3, "k4", 4, "k5", 5, "k6", 6, "k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2, "k3", 3),
+            rbMapOf("k4", 4, "k5", 5, "k6", 6, "k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2, "k3", 3, "k4", 4),
+            rbMapOf("k5", 5, "k6", 6, "k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2, "k3", 3, "k4", 4, "k5", 5),
+            rbMapOf("k6", 6, "k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2, "k3", 3, "k4", 4, "k5", 5, "k6", 6),
+            rbMapOf("k7", 7, "k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+    assertEquals(
+        mergeRBMapsDisallowingOverlap(
+            rbMapOf("k1", 1, "k2", 2, "k3", 3, "k4", 4, "k5", 5, "k6", 6, "k7", 7),
+            rbMapOf("k8", 8, "k9", 9, "k10", 10,
+                "k11", 11, "k12", 12, "k13", 13, "k14", 14, "k15", 15, "k16", 16, "k17", 17, "k18", 18,
+                "k19", 19, "k20", 20)),
+        rbMap20);
+  }
+
+  @Test
+  public void testDuplicateKeys_throws() {
+    Function<String, RBMap<String, Integer>> maker = key1 ->
+        rbMapOf(
+            key1, 1,
+            "B", 2,
+            "C", 3);
+
+    RBMap<String, Integer> doesNotThrow;
+    doesNotThrow = maker.apply("A");
+    doesNotThrow = maker.apply("X");
+    doesNotThrow = maker.apply("Y");
+
+    // the following inserts duplicate keys
+    assertIllegalArgumentException( () -> maker.apply("B"));
+    assertIllegalArgumentException( () -> maker.apply("C"));
   }
 
   @Test
