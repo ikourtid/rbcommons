a45c11ee54938865c3fc534022e213fa9524f73e
Iraklis Kourtidis <iraklis@gmail.com>
2023-05-22 14:06:31 -0700

    added asAlmostExactIntOrThrow

--- src/main/java/com/rb/nonbiz/types/RBIntegers.java
+++ src/main/java/com/rb/nonbiz/types/RBIntegers.java
@@ -1,5 +1,9 @@
 package com.rb.nonbiz.types;
 
+import com.google.common.primitives.Ints;
+import com.rb.nonbiz.util.RBPreconditions;
+import org.apache.commons.math3.complex.RootsOfUnity;
+
 import java.util.OptionalInt;
 
 import static com.rb.nonbiz.collections.RBOptionalTransformers.transformOptionalInt;
@@ -43,6 +47,19 @@ public class RBIntegers {
   public static int minAllowingOptionalInt(int value1, OptionalInt optionalValue2) {
     return transformOptionalInt(optionalValue2, value2 -> Math.min(value1, value2))
         .orElse(value1);
+  }
+
+  /**
+   * If the supplied double is almost round (subject to an epsilon, returns the rounded int value.
+   * Otherwise, an exception is thrown.
+   */
+  public static int asAlmostExactIntOrThrow(double asDouble, Epsilon epsilon) {
+    double rounded = Math.round(asDouble);
+    RBPreconditions.checkArgument(
+        epsilon.valuesAreWithin(rounded, asDouble),
+        "Number %s should be almost an exact int, but the nearest int was %s",
+        asDouble, rounded);
+    return (int) rounded;
   }
 
 }
--- src/test/java/com/rb/nonbiz/types/RBIntegersTest.java
+++ src/test/java/com/rb/nonbiz/types/RBIntegersTest.java
@@ -5,6 +5,9 @@ import org.junit.Test;
 
 import java.util.OptionalInt;
 
+import static com.rb.nonbiz.testutils.Asserters.assertIllegalArgumentException;
+import static com.rb.nonbiz.types.Epsilon.epsilon;
+import static com.rb.nonbiz.types.RBIntegers.asAlmostExactIntOrThrow;
 import static com.rb.nonbiz.types.RBIntegers.maxAllowingOptionalInt;
 import static com.rb.nonbiz.types.RBIntegers.minAllowingOptionalInt;
 import static org.junit.Assert.assertEquals;
@@ -39,6 +42,31 @@ public class RBIntegersTest {
     asserter.accept(OptionalInt.empty(), -33, -33);
     asserter.accept(OptionalInt.empty(),   0,   0);
     asserter.accept(OptionalInt.empty(),  33,  33);
+  }
+
+  @Test
+  public void testAsAlmostExactIntOrThrow() {
+    Epsilon epsilon = epsilon(0.02);
+
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(-1.03, epsilon));
+    assertEquals(-1, asAlmostExactIntOrThrow(-1.01, epsilon));
+    assertEquals(-1, asAlmostExactIntOrThrow(-1.0, epsilon));
+    assertEquals(-1, asAlmostExactIntOrThrow(-0.99, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(-0.97, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(-0.03, epsilon));
+    assertEquals(0, asAlmostExactIntOrThrow(-0.01, epsilon));
+    assertEquals(0, asAlmostExactIntOrThrow(0.0, epsilon));
+    assertEquals(0, asAlmostExactIntOrThrow(0.01, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(0.03, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(0.97, epsilon));
+    assertEquals(1, asAlmostExactIntOrThrow(0.99, epsilon));
+    assertEquals(1, asAlmostExactIntOrThrow(1.0, epsilon));
+    assertEquals(1, asAlmostExactIntOrThrow(1.01, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(1.03, epsilon));
+    assertIllegalArgumentException( () -> asAlmostExactIntOrThrow(1.97, epsilon));
+    assertEquals(2, asAlmostExactIntOrThrow(1.99, epsilon));
+    assertEquals(2, asAlmostExactIntOrThrow(2.0, epsilon));
+    assertEquals(2, asAlmostExactIntOrThrow(2.01, epsilon));
   }
 
 }
