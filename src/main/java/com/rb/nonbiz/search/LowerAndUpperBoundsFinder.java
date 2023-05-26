d3d8f389a93b22693e87c0e84aa668a408adfb79
Stephen Anderson <steve@rowboatadvisors.com>
2023-05-26 09:41:24 -0400

    add version with upper and lower initial points

--- src/main/java/com/rb/nonbiz/search/LowerAndUpperBoundsFinder.java
+++ src/main/java/com/rb/nonbiz/search/LowerAndUpperBoundsFinder.java
@@ -1,6 +1,7 @@
 package com.rb.nonbiz.search;
 
 import com.google.common.collect.Range;
+import com.rb.nonbiz.util.RBPreconditions;
 
 import java.util.function.Function;
 import java.util.function.UnaryOperator;
@@ -9,16 +10,64 @@ import static com.rb.nonbiz.text.SmartFormatter.smartFormat;
 
 public class LowerAndUpperBoundsFinder {
 
-  public <X extends Comparable, Y extends Comparable> Range<X>
-  findLowerAndUpperBounds(Function<X, Y> evaluateInput, X startingPointForSearch, Y target,
-                          UnaryOperator<X> reduceLowerBound,
-                          UnaryOperator<X> increaseUpperBound,
-                          int maxIterations) {
-    X lowerBound = startingPointForSearch;
-    X upperBound = startingPointForSearch;
-    int comparison = evaluateInput.apply(startingPointForSearch).compareTo(target);
+  /**
+   * Find a Range of upper and upper <i>X</i>-value bounds whose <i>Y</i> values bracket a target <i>Y</i> value.
+   *
+   * <p> The user must supply 3 functions: </p>
+   * <ol>
+   *   <li> <i>evaluateInput(x)</i> to evaluate any <i>X</i> values. </li>
+   *   <li> <i>reduceLowerBound(x)</i> to reduce the lower bound. </li>
+   *   <li> <i>increaseUpperBound(x)</i> to increase the upper bound. </li>
+   * </ol>
+   *
+   * <p> The user must also specify the maximum number of bound increases/decreases to use. The same maximum
+   * will be applied for both increasing the upper bound as well as decreasing the lower bound. </p>
+   *
+   * <p> This method should be used if there is a single starting <i>X</i> value for the search.
+   * There is a similar method using an upper and a lower starting range. </p>
+   */
+  public <X extends Comparable<? super X>, Y extends Comparable<? super Y>> Range<X> findLowerAndUpperBounds(
+      Function<X, Y> evaluateInput,
+      X startingPointForSearch,
+      Y target,
+      UnaryOperator<X> reduceLowerBound,
+      UnaryOperator<X> increaseUpperBound,
+      int maxIterations) {
+    // Call the override with separate upper and lower starting points, but with them set to the
+    // same single starting point.
+    return findLowerAndUpperBounds(
+        evaluateInput,
+        startingPointForSearch,
+        startingPointForSearch,
+        target,
+        reduceLowerBound,
+        increaseUpperBound,
+        maxIterations);
+  }
+
+  /**
+   * Return a Range of <i>X</i>-values whose <i>Y</i>-values bracket an input target <i>Y</i>-value.
+   *
+   * <p> As above, but use this if you have separate initial lower and upper <i>X</i> values instead of a single one. </p>
+   */
+  public <X extends Comparable<? super X>, Y extends Comparable<? super Y>> Range<X> findLowerAndUpperBounds(
+      Function<X, Y> evaluateInput,
+      X startingPointForSearchLower,
+      X startingPointForSearchUpper,
+      Y target,
+      UnaryOperator<X> reduceLowerBound,
+      UnaryOperator<X> increaseUpperBound,
+      int maxIterations) {
+    RBPreconditions.checkArgument(
+        startingPointForSearchLower.compareTo(startingPointForSearchUpper) <= 0,
+        "startingPointForSearchLower %s must not be greater than startingPointForSearchUpper %s",
+        startingPointForSearchLower, startingPointForSearchUpper);
+
+    X lowerBound = startingPointForSearchLower;
+    X upperBound = startingPointForSearchUpper;
+    int comparison = evaluateInput.apply(startingPointForSearchUpper).compareTo(target);
     if (comparison > 0) {
-      upperBound = startingPointForSearch;
+      upperBound = startingPointForSearchUpper;
       // keep reducing lowerBound until we get below target, i.e. it becomes a real lower bound
       for (int i = 0; i < maxIterations; i++) {
         lowerBound = reduceLowerBound.apply(lowerBound);
@@ -31,7 +80,7 @@ public class LowerAndUpperBoundsFinder {
           maxIterations, lowerBound, evaluateInput.apply(lowerBound), target));
     } else if (comparison < 0) {
       // keep increasing upperBound until we get above target, i.e. it becomes a real upper bound
-      lowerBound = startingPointForSearch;
+      lowerBound = startingPointForSearchLower;
       for (int i = 0; i < maxIterations; i++) {
         upperBound = increaseUpperBound.apply(upperBound);
         if (evaluateInput.apply(upperBound).compareTo(target) > 0) {
