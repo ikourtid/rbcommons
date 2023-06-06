246f7c910e65883f8d86d7971b22fa0c44f36854
Stephen Anderson <steve@rowboatadvisors.com>
2023-06-06 14:42:51 -0400

    Add missing doc for ClosedUnitFractionRange

--- src/main/java/com/rb/nonbiz/jsonapi/ClosedUnitFractionRangesJsonApiConverter.java
+++ src/main/java/com/rb/nonbiz/jsonapi/ClosedUnitFractionRangesJsonApiConverter.java
@@ -3,17 +3,21 @@ package com.rb.nonbiz.jsonapi;
 import com.google.gson.JsonObject;
 import com.google.inject.Inject;
 import com.rb.nonbiz.collections.ClosedUnitFractionRanges;
+import com.rb.nonbiz.collections.RBSet;
 import com.rb.nonbiz.types.ClosedUnitFractionRange;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import static com.rb.nonbiz.collections.ClosedUnitFractionRanges.closedUnitFractionRanges;
+import static com.rb.nonbiz.collections.RBSet.singletonRBSet;
 import static com.rb.nonbiz.json.JsonValidationInstructions.UNKNOWN_CLASS_OF_JSON_PROPERTY;
 import static com.rb.nonbiz.json.RBGson.jsonDouble;
 import static com.rb.nonbiz.json.RBGson.jsonPercentage;
 import static com.rb.nonbiz.json.RBJsonObjectSimpleConstructors.jsonObject;
 import static com.rb.nonbiz.json.RBJsonObjects.jsonObjectToRBMap;
 import static com.rb.nonbiz.json.RBJsonObjects.rbMapToJsonObject;
+import static com.rb.nonbiz.jsonapi.JsonApiClassDocumentation.JsonApiClassDocumentationBuilder.jsonApiClassDocumentationBuilder;
 import static com.rb.nonbiz.jsonapi.JsonApiClassWithNonFixedPropertiesDocumentation.JsonApiClassWithNonFixedPropertiesDocumentationBuilder.jsonApiClassWithNonFixedPropertiesDocumentationBuilder;
 import static com.rb.nonbiz.text.HumanReadableDocumentation.documentation;
 import static com.rb.nonbiz.text.Strings.asSingleLineWithNewlines;
@@ -70,6 +74,29 @@ public class ClosedUnitFractionRangesJsonApiConverter implements HasJsonApiDocum
                 "min", jsonDouble(15.0),
                 "max", jsonDouble(35.0))))
         .build();
+  }
+
+  @Override
+  public Optional<RBSet<JsonApiDocumentation>> getAdditionalJsonApiDocumentation() {
+    return Optional.of(singletonRBSet(
+        jsonApiClassDocumentationBuilder()
+            .setClass(ClosedUnitFractionRange.class)
+            .setSingleLineSummary(documentation(
+                "A `Range` of `UnitFraction`s."))
+            .setLongDocumentation(documentation(asSingleLineWithNewlines(
+                "A `Range` with closed upper and lower bounds, each with a value",
+                "between 0% and 100%. <p />",
+                "The range minimum is specified via <b>min</b> and the range maximum by <b>max</b>.",
+                "Both <b>min</b> and <b>max</b> are given as percentages (0-100).")))
+            // This converter does not use a JsonValidationInstructions, deferring all validation to
+            // RangeJsonApiConverter. However, we include its RANGE_JSON_VALIDATION_INSTRUCTIONS here
+            // to help with the JsonApiDocumentation.
+            .setJsonValidationInstructions(RangeJsonApiConverter.RANGE_JSON_VALIDATION_INSTRUCTIONS)
+            .hasSingleChildJsonApiConverter(rangeJsonApiConverter)
+            .setNontrivialSampleJson(jsonObject(
+                "min", jsonDouble(10.0),
+                "max", jsonDouble(30.0)))
+            .build()));
   }
 
 }
