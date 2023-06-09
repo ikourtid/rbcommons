713e02e2ea6117035f950ab03ae6111588ff0ed2
Stephen Anderson <steve@rowboatadvisors.com>
2023-06-09 12:54:44 -0400

    add toJson(), fromJson()

--- src/main/java/com/rb/nonbiz/jsonapi/RBCategoryMapJsonApiConverter.java
+++ src/main/java/com/rb/nonbiz/jsonapi/RBCategoryMapJsonApiConverter.java
@@ -4,20 +4,23 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.inject.Inject;
 import com.rb.nonbiz.collections.RBCategoryMap;
-import com.rb.nonbiz.json.JsonPropertySpecificDocumentation;
 import com.rb.nonbiz.json.JsonValidationInstructions;
 import com.rb.nonbiz.json.JsonValidator;
 
 import java.util.function.Function;
 
+import static com.rb.nonbiz.collections.RBCategoryMap.rbCategoryMap;
 import static com.rb.nonbiz.collections.RBMapSimpleConstructors.rbMapOf;
-import static com.rb.nonbiz.collections.RBMapSimpleConstructors.singletonRBMap;
 import static com.rb.nonbiz.json.JsonApiPropertyDescriptor.RBMapJsonApiPropertyDescriptor.rbMapJsonApiPropertyDescriptor;
 import static com.rb.nonbiz.json.JsonApiPropertyDescriptor.SimpleClassJsonApiPropertyDescriptor.simpleClassJsonApiPropertyDescriptor;
 import static com.rb.nonbiz.json.JsonPropertySpecificDocumentation.jsonPropertySpecificDocumentation;
 import static com.rb.nonbiz.json.JsonValidationInstructions.JsonValidationInstructionsBuilder.jsonValidationInstructionsBuilder;
 import static com.rb.nonbiz.json.JsonValidationInstructions.UNKNOWN_CLASS_OF_JSON_PROPERTY;
-import static com.rb.nonbiz.json.JsonValidationInstructions.UNKNOWN_DATA_CLASS_JSON_API_DESCRIPTOR;
+import static com.rb.nonbiz.json.RBJsonObjectBuilder.rbJsonObjectBuilder;
+import static com.rb.nonbiz.json.RBJsonObjectGetters.getJsonElementOrThrow;
+import static com.rb.nonbiz.json.RBJsonObjectGetters.getJsonObjectOrThrow;
+import static com.rb.nonbiz.json.RBJsonObjects.jsonObjectToRBMap;
+import static com.rb.nonbiz.json.RBJsonObjects.rbMapToJsonObject;
 
 /**
  * Converts an {@link RBCategoryMap} back and forth to JSON for our public API.
@@ -45,15 +48,35 @@ public class RBCategoryMapJsonApiConverter implements HasJsonApiDocumentation {
       RBCategoryMap<K, V> rbCategoryMap,
       Function<K, String> keySerializer,
       Function<V, JsonElement> valueSerializer) {
-    return null;
+    return jsonValidator.validate(
+        rbJsonObjectBuilder()
+            .setJsonElement(
+                "valueRegardlessOfCategory",
+                valueSerializer.apply(rbCategoryMap.getValueRegardlessOfCategory()))
+            .setJsonSubObject(
+                "categoryMap",
+                rbMapToJsonObject(
+                    rbCategoryMap.getRawMap(),
+                    keySerializer,
+                    valueSerializer))
+            .build(),
+        JSON_VALIDATION_INSTRUCTIONS);
   }
 
   public <K, V> RBCategoryMap<K, V> fromJsonObject(
       JsonObject jsonObject,
       Function<String, K> keyDeserializer,
       Function<JsonElement, V> valueDeserializer) {
+    jsonValidator.validate(jsonObject, JSON_VALIDATION_INSTRUCTIONS);
 
-    return null;
+    return rbCategoryMap(
+        valueDeserializer.apply(
+            getJsonElementOrThrow(
+                jsonObject, "valueRegardlessOfCategory")),
+        jsonObjectToRBMap(
+            getJsonObjectOrThrow(jsonObject, "categoryMap"),
+            keyDeserializer,
+            valueDeserializer));
   }
 
   @Override
