c0610def00460e5c2661bbc45da029377ee98885
Stephen Anderson <steve@rowboatadvisors.com>
2023-10-08 22:16:05 -0400

    update

--- src/main/java/com/rb/nonbiz/jsonapi/CoordinatesJsonApiConverter.java
+++ src/main/java/com/rb/nonbiz/jsonapi/CoordinatesJsonApiConverter.java
@@ -8,8 +8,11 @@ import java.util.Arrays;
 import static com.rb.nonbiz.collections.Coordinates.coordinates;
 import static com.rb.nonbiz.json.RBGson.jsonInteger;
 import static com.rb.nonbiz.json.RBJsonArrays.iteratorToJsonArray;
+import static com.rb.nonbiz.json.RBJsonArrays.jsonArray;
 import static com.rb.nonbiz.json.RBJsonArrays.jsonArrayToIntArray;
 import static com.rb.nonbiz.jsonapi.JsonApiClassDocumentation.JsonApiClassDocumentationBuilder.jsonApiClassDocumentationBuilder;
+import static com.rb.nonbiz.text.HumanReadableDocumentation.documentation;
+import static com.rb.nonbiz.text.Strings.asSingleLineWithNewlines;
 
 /**
  * Converts {@link Coordinates} back and forth to JSON for our public API.
@@ -33,6 +36,15 @@ public class CoordinatesJsonApiConverter implements HasJsonApiDocumentation {
   @Override
   public JsonApiDocumentation getJsonApiDocumentation() {
     return jsonApiClassDocumentationBuilder()
+        .setClass(Coordinates.class)
+        .setSingleLineSummary(documentation(
+            "Holds the coordinates of a point in an N-dimensional grid."))
+        .setLongDocumentation(documentation(asSingleLineWithNewlines(
+            "Each coordinate must be a non-negative integer.")))
+        .hasNoJsonValidationInstructions()
+        .hasNoChildJsonApiConverters()
+        .setNontrivialSampleJson(
+            jsonArray(jsonInteger(4), jsonInteger(0), jsonInteger(1), jsonInteger(0)))
         .build();
   }
 
