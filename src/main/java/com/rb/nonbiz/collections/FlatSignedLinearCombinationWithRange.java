17874279f8977095a8857e847e9a7e2595a618d4
Iraklis Kourtidis <iraklis@gmail.com>
2024-10-19 10:42:35 -0400

    comment

--- src/main/java/com/rb/nonbiz/collections/FlatSignedLinearCombinationWithRange.java
+++ src/main/java/com/rb/nonbiz/collections/FlatSignedLinearCombinationWithRange.java
@@ -1,32 +1,26 @@
 package com.rb.biz.investing.strategy.optbased.fullopt;
 
-import com.google.common.base.Joiner;
 import com.google.common.collect.BoundType;
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Range;
 import com.rb.biz.marketdata.instrumentmaster.InstrumentMaster;
 import com.rb.biz.types.asset.AssetId;
 import com.rb.nonbiz.collections.FlatSignedLinearCombination;
-import com.rb.nonbiz.collections.RBRanges;
-import com.rb.nonbiz.math.eigen.Investable;
-import com.rb.nonbiz.math.optimization.general.RawVariable;
 import com.rb.nonbiz.text.PrintsInstruments;
 import com.rb.nonbiz.text.Strings;
-import com.rb.nonbiz.types.WeightedBySignedFraction;
 import com.rb.nonbiz.util.RBPreconditions;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 
 import static com.rb.nonbiz.collections.RBRanges.hasEitherBoundOpen;
 import static com.rb.nonbiz.collections.RBRanges.rangeIsUnrestricted;
 import static com.rb.nonbiz.text.Strings.formatRange;
 
 /**
- * Represents a general expression such as e.g. {@literal 0.4 * AAPL - 0.1 * GOOG > 12.34 }
+ * Represents a general expression such as e.g. {@literal 0.4 * AAPL - 0.1 * GOOG > 12.34 } (although it's generic,
+ * so the value type could be anything).
  *
- * <p> Although this data class is general, it is intended to convert eventually to a constraint inside an
+ * <p> Although this data class is general, its first use is to have it convert eventually to a constraint inside an
  * optimization.
  * Therefore, the supplied {@link Range} cannot be fully unrestricted (because that wouldn't translate to a
  * constraint). Also, for any (or both) endpoints supplied, they have to be {@link BoundType#CLOSED} for
@@ -35,7 +29,7 @@ import static com.rb.nonbiz.text.Strings.formatRange;
  *
  * <p> Note that this class can be used to generate a constraint, but is not intended to directly represent a
  * constraint. That is, the value type in the generic specification could be e.g. {@link AssetId},
- * not necessarily some low-level class such as {@link RawVariable} that is directly involved in an
+ * not necessarily some low-level class such as RawVariable (in higher repo) that is directly involved in an
  * optimization. </p>
  */
 public class FlatSignedLinearCombinationWithRange<T> {
